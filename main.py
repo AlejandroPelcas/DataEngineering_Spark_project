9ad74da1bbca4d6908b496b4f91ac88a6b294088
AlejandroPelcas <alejandro.pelcas@berkeley.edu>
2024-12-27 09:37:49 -0800

    Read data
    Able to print out data of mnm file

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import sys
 from pyspark.sql.functions import *
 
+os.environ["JAVA_HOME"] = "/Users/alejandropelcastre/Library/Java/JavaVirtualMachines/openjdk-22.0.1/Contents/Home"
+
+
 # Create a spark context
 spark_conf = SparkConf()\
   .setAppName("YourTest")\
@@ -12,4 +15,6 @@ sc = SparkContext.getOrCreate(spark_conf)
 
 # SparkSession is the entry point for programming with the DataFrame and Dataset APIs.
 spark = SparkSession(sc)
-
+df = spark.read.csv("mnm_dataset.csv" , header=True, inferSchema=True)
+x = df.take(3)
+print(x)
